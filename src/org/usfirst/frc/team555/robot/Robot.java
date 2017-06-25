dcd4cc1644cc46650200c915a3b58534c9641b64
rich kopelow <rich.kopelow@gmail.com>
2017-06-24 21:46:43 -0400

    Breakout of Steamworks code for cyborg

---
+++ src/org/usfirst/frc/team555/robot/Robot.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team555.robot;
+
+import org.usfirst.frc.team555.steamworks.SWRobot;
+
+/**
+ * 
+ * This is the main robot definition class.
+ * 
+ */
+public class Robot extends SWRobot {
+	
+}
